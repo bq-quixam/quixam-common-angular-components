1664d1af96e8e1e607bc3b69afcff642fb9d30b3
rakesh <rakeshuvsn@users.noreply.github.com>
2017-10-08 10:01:31 -0400

    initial setup is done

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quixamCommonAngularComponentsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the quixamCommonAngularComponentsApp
+ */
+angular.module('quixamCommonAngularComponentsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quixamCommonAngularComponentsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the quixamCommonAngularComponentsApp
+ */
+angular.module('quixamCommonAngularComponentsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
